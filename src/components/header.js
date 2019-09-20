b0bae7984051646d981ec6a9962d7096f5dadcf2
Gustavo Nering <gstvdesign@gmail.com>
2019-09-20 07:07:52 -0300

    typo

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Value from "../components/value"
 export default () => (
 	<div>
 		<img src="logo.png" alt=""/>
-		<Value valueText="Eu contruo sites e catálogos para empresas de móveis que encantam lojistas e chamam atenção dos seus clientes."></Value>
+		<Value valueText="Eu construo sites e catálogos para empresas de móveis que encantam lojistas e chamam atenção dos seus clientes."></Value>
 	  <p>Se você tem um time de venda e quer ter mais presença nas lojas, o Benefi cria sites e catálogos com
 		   visual poderoso para vender seus produtos. </p>
 	</div>
